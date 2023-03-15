c2056bb4bd9060d975cf07331169826e32622110
HP <HP@LAPTOP-P0J4POGC>
2023-03-15 16:39:41 +0530

    First Commit

---
+++ C2TC_VVIT/src/com/tnsif/prog/FirstProgram.java
@@ -0,0 +1,13 @@
+package com.tnsif.prog;
+/*
+ *  Program to demonstrate Hello World
+ */
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub // Single Line Comment
+		System.out.println("Hello");
+
+	}
+
+}
---
+++ C2TC_VVIT/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author HP
+ *
+ */
+module C2TC_VVIT {
+}
