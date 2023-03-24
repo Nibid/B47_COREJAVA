87c1a065931aef75832dbf0ca75662ebf43fbfc4
HP <HP@LAPTOP-P0J4POGC>
2023-03-24 18:12:25 +0530

    final keyword

---
+++ C2TC_VVIT/src/com/tnsif/staticmethod/Abc.java
@@ -0,0 +1,17 @@
+package com.tnsif.staticmethod;
+
+class Xyz {
+	final void display() {
+		
+	}
+}
+public class Abc extends Xyz{
+	final int a=10;
+	Abc() {
+		//a++;
+	}
+//	final void display() {
+//	Cannot override the final method from Xyz
+//	}
+
+}
