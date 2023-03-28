4814fe0acc039f32487f61508250d07cd72d60f2
HP <HP@LAPTOP-P0J4POGC>
2023-03-28 18:42:26 +0530

    Nested Interface

---
+++ C2TC_VVIT/src/com/thsif/nestedinterface/MyClass.java
@@ -0,0 +1,19 @@
+package com.thsif.nestedinterface;
+
+public class MyClass implements Outer{// implements only outer class
+
+	@Override
+	public void m1() {
+		// TODO Auto-generated method stub
+		
+	}
+
+//public class MyClass implements Outer.inner{// implements only inner class
+//
+//	@Override
+//	public void m2() {
+//		// TODO Auto-generated method stub
+//		
+//	}
+
+}
---
+++ C2TC_VVIT/src/com/thsif/nestedinterface/Outer.java
@@ -0,0 +1,10 @@
+package com.thsif.nestedinterface;
+
+public interface Outer {
+	void m1();
+	interface inner {
+		void m2();
+	
+	}
+
+}
