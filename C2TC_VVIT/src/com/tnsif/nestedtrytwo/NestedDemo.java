62ea5838a5b51673bc78d97b650133108a5e8391
HP <HP@LAPTOP-P0J4POGC>
2023-03-31 15:56:16 +0530

    Arithmetic and NullPointer Exception

--- C2TC_VVIT/src/com/tnsif/nestedtrytwo/NestedDemo.java
+++ C2TC_VVIT/src/com/tnsif/nestedtrytwo/NestedDemo.java
@@ -6,11 +6,13 @@ public class NestedDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// Initializing the variable x and y from the user
 		Scanner ob = new Scanner(System.in);
 		System.out.println("Enter the value of x and y\n");
 		int x = ob.nextInt();
 		int y = ob.nextInt();
 		
+		// Initializing String variable with null value
         String ptr = null;
         
 		System.out.println("value of x and y "+x+" "+y);
--- C2TC_VVIT/src/com/tnsif/nestedtrytwo/NestedTry.java
+++ C2TC_VVIT/src/com/tnsif/nestedtrytwo/NestedTry.java
@@ -1,25 +1,33 @@
 package com.tnsif.nestedtrytwo;
-
+/*
+ * demonstrate that invoking ArithmeticException & NullPointerException nested
+ */
 public class NestedTry {
 	int nestedtry(int x, int y, String ptr)
 	{
 		int z = 0;
+		// outer (main) try block  
 		try
 	    {
-			if (ptr.equals("gfg"))
+			// Checking if ptr.equals null or works fine.
+			if (ptr.equals("Hello"))
 	            System.out.println("Same");
 	        else
 	            System.out.println("Not Same");
 			
+			//inner try block
 			try {
 				z = x/y;
 				System.out.println(z);
 			}
+			// to handles ArithmeticException 
 			catch(ArithmeticException ar) {
 				System.out.println("ArithmeticException");
 			}
 			
 	    }
+		// This line of code throws NullPointerException
+        // because ptr is null
 	    catch(NullPointerException e)
 	    {
 	        System.out.println("NullPointerException");
