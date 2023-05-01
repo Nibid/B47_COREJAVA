92b7dda3785f726a68271df5f068164bcd8cfa8f
HP <HP@LAPTOP-P0J4POGC>
2023-05-01 18:34:36 +0530

    Factory Pattern Demo

--- C2TC_VVIT/src/com/tnsif/factorypattern/Demo.java
+++ C2TC_VVIT/src/com/tnsif/factorypattern/Demo.java
@@ -4,6 +4,11 @@ public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Vehicle car = VehicleFactory.getInstance("car", 4);
+		System.out.println(car);
+		Vehicle bike = VehicleFactory.getInstance("bike", 2);
+		System.out.println(bike);
 
 	}
 
